4807c4581946851a6d2d77dc6024da3b54c004f4
elhabeeb337 <97795946+elhabeeb337@users.noreply.github.com>
2022-01-22 08:58:37 +0200

    Update delivery_app.dart

--- delivery_app.dart
+++ delivery_app.dart
@@ -1,60 +1,105 @@
+import 'dart:math';
 import 'dart:io';
 
+//calculate delivery fee total
+
+  double feeCalc(
+    double cartValue, double distance, int itemsNum, DateTime transactionTime) {
+  double totalDeliveryFee = 0.0;
+  const double kMaxDeliveryFee = 15;
+
+
+  //Adding surcharge when cartValue < 10
+  double surchargeCartValue(double cartValue) {
+    const double kMinDeliveryFee = 10;
+
+    if (cartValue < kMinDeliveryFee) {
+      return kMinDeliveryFee - cartValue;
+    }
+    return 0.0;
+  }
+
+  // Distance fee calculation
+  double distanceFee(double distance) {
+    const double kMinDistance = 1000;
+    const double kMinDistanceFee = 2;
+    const double kAddDistance = 500;
+
+  if (distance < kMinDistance) {
+    return kMinDistanceFee;
+  } 
+  return kMinDistanceFee + ((distance - kMinDistance) / kAddDistance).ceil();
+  }
+
+  //Adding Surcharge when itemNum > 4
+  double surchargeOnItemsNum(int itemsNum) {
+    double itemSurcharge = 0.0;
+    const double kItemsurcharge = 0.50;
+
+    if (itemsNum > 4) {
+      itemSurcharge = ((itemsNum - 4) * kItemsurcharge);
+      
+      return itemSurcharge;
+  }
+  return itemSurcharge;
+}
+
+//Friday rush hour
+bool isRushHr(DateTime transactionTime) {
+  bool isFridayRush = false;
+
+  if ((transactionTime.weekday == DateTime.friday) &&
+      (15 <= transactionTime.hour) &&
+      (transactionTime.hour <= 19)) {
+    isFridayRush = true;
+  }
+
+  return isFridayRush;
+}
+//First senario (no cart value or item to deliver)
+  if (cartValue == 0.0 && itemsNum == 0.0) {
+    return totalDeliveryFee;
+  }
+  //Second senario (with cartValue < 100)
+  if (cartValue < 100) {
+    totalDeliveryFee = surchargeCartValue(cartValue) +
+        distanceFee(distance) +
+        surchargeOnItemsNum(itemsNum);
+
+    //Third senario (Friday rush hour)
+    bool rushHr = isRushHr(transactionTime);
+    if (rushHr) {
+      totalDeliveryFee = totalDeliveryFee * 1.1;
+    }
+  }
+  return min(totalDeliveryFee, kMaxDeliveryFee);
+    }
+
+//import 'delivery_app.dart';
+
+// The variables used to calculate the Fee.
+double cartValue = 0.0, distance = 0.0;
+int itemsNum = 0;
+
 void main() {
-  print("enter unit of item");
-  int? unit = int.parse(stdin.readLineSync()!);
 
-  print("enter price of item");
-  double? price = double.parse(stdin.readLineSync()!);
+   print("Enter cart value");
+  cartValue = double.parse(stdin.readLineSync()!);
 
-  double cartvalue = (unit * price);
-  if (cartvalue <= 10) {
-    print(10);
-  } else {
-    print((cartvalue));
-  }
+  print("Enter the amount of items");
+  itemsNum = int.parse(stdin.readLineSync()!);
 
-  print("enter store to customer distance in meters");
-  int? distance = int.parse(stdin.readLineSync()!);
-  const int basefee = 2;
+  print("Enter distance in meters");
+  distance = double.parse(stdin.readLineSync()!);
 
-  bool isUnitGreaterThan5 = true;
-  bool isDeliveryFeelessThan15 = true;
-  bool extraunit = unit > 4;
+  print("Enter delivery date and time in the format YYYYMMDD HHMMSS");
+  DateTime transactionTime = DateTime.parse(stdin.readLineSync()!);
 
-  if (cartvalue >= 100) {
-    print("no delivery cost");
-  } else if (isUnitGreaterThan5) {
-    if (isDeliveryFeelessThan15) {
-      if (extraunit) {
-        print((basefee + ((distance / 500).ceil() - 2)) + ((unit - 4) * 0.5));
-      } else {
-        print(basefee + (distance / 500).ceil() - 2);
-      }
-    }
-  }
-  print("enter day the week");
-  int? dayhour = int.parse(stdin.readLineSync()!);
-  final int friday = 5;
-  var rushhour = DateTime(5);
-  bool isrushhour = true;
+  // to calculate delivery fee
+  double totalDeliveryFee = feeCalc(cartValue, distance, itemsNum, transactionTime);
+  print("Delivery totalDeliveryFee is $totalDeliveryFee");
+}
 
-//
-  //var time = DateTime.now();
-  //var fridayrush = DateTime.friday;
-  //bool isrushhour = true;
-  //bool isfridayrush = true;
 
-  if (isUnitGreaterThan5) {
-    if (isDeliveryFeelessThan15) {
-      if (extraunit) {
-        if (isrushhour) {
-          print((basefee + ((distance / 500).ceil() - 2)) +
-              ((unit - 4) * 0.5) * 1.1);
-        }
-      } else if (isDeliveryFeelessThan15 && isrushhour) {
-          print(((basefee + (distance / 500).ceil() - 2) * 1.1));
-        }  
-      } 
-    }
-  }
+
+
