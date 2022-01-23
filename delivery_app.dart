d3b5d6d60f5fbdae196622ecd6777a354fd04cd6
elhabeeb337 <97795946+elhabeeb337@users.noreply.github.com>
2022-01-23 08:48:56 +0200

    Update delivery_app.dart

--- delivery_app.dart
+++ delivery_app.dart
@@ -75,31 +75,3 @@ bool isRushHr(DateTime transactionTime) {
   return min(totalDeliveryFee, kMaxDeliveryFee);
     }
 
-//import 'delivery_app.dart';
-
-// The variables used to calculate the Fee.
-double cartValue = 0.0, distance = 0.0;
-int itemsNum = 0;
-
-void main() {
-
-   print("Enter cart value");
-  cartValue = double.parse(stdin.readLineSync()!);
-
-  print("Enter the amount of items");
-  itemsNum = int.parse(stdin.readLineSync()!);
-
-  print("Enter distance in meters");
-  distance = double.parse(stdin.readLineSync()!);
-
-  print("Enter delivery date and time in the format YYYYMMDD HHMMSS");
-  DateTime transactionTime = DateTime.parse(stdin.readLineSync()!);
-
-  // to calculate delivery fee
-  double totalDeliveryFee = feeCalc(cartValue, distance, itemsNum, transactionTime);
-  print("Delivery totalDeliveryFee is $totalDeliveryFee");
-}
-
-
-
-
