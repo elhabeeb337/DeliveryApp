f39dbbdbbc80832b0e1fee429a78902ffa6ecbf0
elhabeeb337 <97795946+elhabeeb337@users.noreply.github.com>
2022-01-25 17:28:25 +0200

    Update delivery_app.dart

--- delivery_app.dart
+++ delivery_app.dart
@@ -1,62 +1,12 @@
 import 'dart:math';
-import 'dart:io';
 
 //calculate delivery fee total
 
-  double feeCalc(
+double feeCalc(
     double cartValue, double distance, int itemsNum, DateTime transactionTime) {
   double totalDeliveryFee = 0.0;
   const double kMaxDeliveryFee = 15;
-
-
-  //Adding surcharge when cartValue < 10
-  double surchargeCartValue(double cartValue) {
-    const double kMinDeliveryFee = 10;
-
-    if (cartValue < kMinDeliveryFee) {
-      return kMinDeliveryFee - cartValue;
-    }
-    return 0.0;
-  }
-
-  // Distance fee calculation
-  double distanceFee(double distance) {
-    const double kMinDistance = 1000;
-    const double kMinDistanceFee = 2;
-    const double kAddDistance = 500;
-
-  if (distance < kMinDistance) {
-    return kMinDistanceFee;
-  } 
-  return kMinDistanceFee + ((distance - kMinDistance) / kAddDistance).ceil();
-  }
-
-  //Adding Surcharge when itemNum > 4
-  double surchargeOnItemsNum(int itemsNum) {
-    double itemSurcharge = 0.0;
-    const double kItemsurcharge = 0.50;
-
-    if (itemsNum > 4) {
-      itemSurcharge = ((itemsNum - 4) * kItemsurcharge);
-      
-      return itemSurcharge;
-  }
-  return itemSurcharge;
-}
-
-//Friday rush hour
-bool isRushHr(DateTime transactionTime) {
-  bool isFridayRush = false;
-
-  if ((transactionTime.weekday == DateTime.friday) &&
-      (15 <= transactionTime.hour) &&
-      (transactionTime.hour <= 19)) {
-    isFridayRush = true;
-  }
-
-  return isFridayRush;
-}
-//First senario (no cart value or item to deliver)
+  //First senario (no cart value or item to deliver)
   if (cartValue == 0.0 && itemsNum == 0.0) {
     return totalDeliveryFee;
   }
@@ -74,4 +24,52 @@ bool isRushHr(DateTime transactionTime) {
   }
   return min(totalDeliveryFee, kMaxDeliveryFee);
     }
+
+  //Adding surcharge when cartValue < 10
+  double surchargeCartValue(double cartValue) {
+    const double kMinDeliveryFee = 10;
+
+    if (cartValue < kMinDeliveryFee) {
+      return kMinDeliveryFee - cartValue;
+    }
+    return 0.0;
+  }
+
+  // Distance fee calculation
+  double distanceFee(double distance) {
+    const double kMinDistance = 1000;
+    const double kMinDistanceFee = 2;
+    const double kAddDistance = 500;
+
+    if (distance < kMinDistance) {
+      return kMinDistanceFee;
+    }
+    return kMinDistanceFee + ((distance - kMinDistance) / kAddDistance).ceil();
+  }
+
+  //Adding Surcharge when itemNum > 4
+  double surchargeOnItemsNum(int itemsNum) {
+    double itemSurcharge = 0.0;
+    const double kItemsurcharge = 0.50;
+
+    if (itemsNum > 4) {
+      itemSurcharge = ((itemsNum - 4) * kItemsurcharge);
+
+      return itemSurcharge;
+    }
+    return itemSurcharge;
+  }
+
+//Friday rush hour
+  bool isRushHr(DateTime transactionTime) {
+    bool isFridayRush = false;
+
+    if ((transactionTime.weekday == DateTime.friday) &&
+        (15 <= transactionTime.hour) &&
+        (transactionTime.hour <= 19)) {
+      isFridayRush = true;
+    }
+
+    return isFridayRush;
+  }
 
